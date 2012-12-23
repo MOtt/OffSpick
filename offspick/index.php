90deaab4ff833d1c3d3f2126326ae8ed9dd1adf8
MOtt <ottmic02@students.zhaw.ch>
2012-12-23 20:37:13 +0100

    Backend zur Ausgabe der Kontakte

---
+++ offspick/index.php
@@ -0,0 +1,66 @@
+<?php
+
+header("Content-Type: application/json; charset=utf-8");
+
+/* Verbindzung zu SQL-Server */
+mysql_connect('localhost', 'mobapp', 'fw118') or die('Keine Verbindung möglich: ' . mysql_error());
+
+/* Datenbank auswählen */
+mysql_select_db("OffSpick");
+
+/* Collation anpassen */
+mysql_query("SET NAMES 'utf8' COLLATE 'utf8_general_ci'") or die(mysql_error());
+
+
+$func = $_REQUEST["func"];
+
+	switch($func){
+		case "offSpickList": offSpickList($_REQUEST); break;
+		default: doerror("hmmm* - diese Funktion wurde noch nicht definiert");
+	}
+
+
+function offSpickList($param){
+
+	$entries = array();
+	$datetoday = date("Y-m-d");
+
+	$sqlstring = "select cc.categoryID,ca.Text,ca.Description,cc.ContactID,c.Name,c.Description,cn.Phone from OffSpick.ContactCategory cc inner join OffSpick.Category ca on ca.ID=cc.CategoryID inner join OffSpick.Contact c on c.ID = cc.ContactID inner join OffSpick.ContactNumber cn on cc.ContactID = cn.ID where '".$datetoday."' between ca.ValidFrom and ca.ValidTo and '".$datetoday."' between c.ValidFrom and c.ValidTo order by ca.Sort,c.Sort";
+
+
+	$result = mysql_query($sqlstring);
+
+	while ($row = mysql_fetch_array($result, MYSQL_BOTH)) {
+  	  $row_array['categoryId'] = (int)$row[0];
+	  $row_array['categoryName'] = $row[1];
+  	  $row_array['categoryDescription'] = $row[2];
+	  $row_array['contactId'] = (int)$row[3];
+  	  $row_array['contactName'] = $row[4];
+   	  $row_array['contactDescription'] = $row[5];
+	  $row_array['contactNumber'] = $row[6];
+	  array_push($entries,$row_array);
+	}
+
+	doOutput($entries);
+}
+
+
+function doError($msg){
+	doOutput(null,false,$msg);
+}
+
+
+function doOutput($result,$sucess=true,$msg=null){
+
+	header ("Content-Type: application/json");
+
+	$result = array(
+				'sucess' => $sucess,
+				'error' => $msg,
+				'result' => $result
+				);
+
+	echo (json_encode($result));
+}
+
+?>

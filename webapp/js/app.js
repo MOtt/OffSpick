d3abb1773bda7dcaad00590fab8c401a288579da
MOtt <ottmic02@students.zhaw.ch>
2012-12-29 02:04:26 +0100

    kompletter Stand mit Login und Fehlerhandling

--- webapp/js/app.js
+++ webapp/js/app.js
@@ -1,26 +1,24 @@
-function callBack(){
-	};
-
+// ***************************************************************************************************
+// Daten aus LocalStorage für Listendarstellung aufbereiten
+// ***************************************************************************************************
 
 function offSpickListInsert(param){
-
 
 var offSpickList = $('#offSpickList');
 var $currentCategory = null;
 var i,j,li, ul;
 
-try{
-	
-	for (i = 0; i < param.result.length; i++) {
-		if ($currentCategory != param.result[i].categoryName){
+try{	
+	for (i = 0; i < param.length; i++) {
+		if ($currentCategory != param[i].categoryName){
 			li = $('<li></li>');
-			li.append('<h3>'+param.result[i].categoryName+'</h3><p>'+param.result[i].categoryDescription+'</p>');
-			$currentCategory = param.result[i].categoryName;
+			li.append('<h3>'+param[i].categoryName+'</h3><p>'+param[i].categoryDescription+'</p>');
+			$currentCategory = param[i].categoryName;
 		ul = $('<ul></ul>');
 		}
 
-		if ($currentCategory == param.result[i].categoryName) {
-			ul.append('<li data-icon="grid"><a data-id="'+param.result[i].contactId+'" href="tel:'+param.result[i].contactNumber+'">'+param.result[i].contactName+'<p>'+param.result[i].contactDescription+'</p></a></li>');
+		if ($currentCategory == param[i].categoryName) {
+			ul.append('<li data-icon="grid"><a data-id="'+param[i].contactId+'" href="tel:'+param[i].contactNumber+'">'+param[i].contactName+'<p>'+param[i].contactDescription+'</p></a></li>');
 		}
 		li.append(ul);
 		offSpickList.append(li);
@@ -35,61 +33,37 @@ catch(e){
 }	
 
 
-
-function loadData(){
-
-// Variable muss durch PhoneGap-Funktion gelöst werden (On-/Offline)
-var online = true;
-
-if (online==true){
-	
-	
-	$.ajax({
-  	 type: 'POST',
- 	 url: '../offspick/',
- 	 data: {'func': 'offSpickList'},
- 	 success: function(data){offSpickListInsert(data); callBack()}
-	 });
-	
-}
-else {
-	
-	callBack();
-}
-
-};
-
-
 $(document).bind('mobileinit',function(){						   
-
-	$('#offSpickListPage').live('pageinit',function(toPage,options){
-	console.log(toPage,options);
-	loadData();
+	console.log('MobileInit');
+	$('#offSpickListPage').live('pageinit',function(toPage,options){	 
+		checkLoginData();									 
 	});
-
-    $.mobile.page.prototype.options.addBackBtn = true;
-	$.mobile.page.prototype.options.backBtnText = "Home";
-
+	
+	$('#loginForm').live('pageinit',function(toPage,options){		 
+		$('#loginBtn').live('click',saveLogin);									 
+	});
 });
 
 
 $(document).ready(function(){
-   
- });
+});
  
  
-// *******************************************************
-// ab hier folgen die LocalStorage-Funktionen
-// *******************************************************
+// ***************************************************************************************************
+// Logindaten lokal auf Existenz prüfen. Falls OffUser oder OffPassword fehlt -> Loginmaske anzeigen
+// ***************************************************************************************************
 	
-function checkLogin() {
+function checkLoginData() {
 	
-	try {         
+	// Prüfen ob Login im Localstorage eingetragen
+	
+	try {   
 		var loginuser = localStorage.getItem('OffUser');
 		var loginpassword = localStorage.getItem('OffPassword'); 
 		
 		if ((loginuser.length > 0) && (loginpassword.length > 0)){
-			console.log('User/Passwort vorhanden: '+loginuser+' / '+loginpassword);
+			$.mobile.changePage("#offSpickListPage"); 
+			requestSession()	
 		}  
 	}
 	catch (e) {        
@@ -97,11 +71,39 @@ function checkLogin() {
 			console.log("Fehler" + e);         
 			}
 	else {             
-		console.log('Benutzer / Passwort unvollständig');         
+		alert("Benutzername oder Passwort ist leer");
+		$.mobile.changePage("#loginForm"); 
 		}     
 	}  
 } 	
 	
+	
+// ***************************************************************************************************
+// Benutzerdaten im LocalStorage ablegen (Key's: OffUser resp. OffPassword
+// ***************************************************************************************************	
+
+function saveLogin() {
+	                  
+	try {         
+		localStorage.setItem("OffUser", $("#user").val());   
+		localStorage.setItem("OffPassword", $("#pw").val()); 
+		$.mobile.changePage("#offSpickListPage"); 
+		requestSession()
+		}      
+	catch (e) {        
+		if (e == "QUOTA_EXCEEDED_ERR") {             
+			console.log("Fehler: LocalStorage voll");         
+			}         
+	else {             
+		console.log("Fehler: Speichern in LocalStorage nicht möglich");         
+		}     
+	}  
+}
+
+
+// ***************************************************************************************************
+// Prüfen ob LocalStorage verfügbar ist. Sonst Fehlermeldung
+// ***************************************************************************************************
 
 function localStorageLoad() {              
 	if (typeof(Storage) == "undefined" ) {             
@@ -109,7 +111,84 @@ function localStorageLoad() {
 	}      
 	else {         
 		console.log("LocalStorage und SessionStorage werden unterstützt.");     
-		checkLogin();
+		checkLoginData();
 		}
 }
 
+// ***************************************************************************************************
+// Logindaten prüfen. Falls korrekt -> Session aufbauen, sonst Login-Formular anzeigen
+// ***************************************************************************************************
+
+function requestSession() {
+
+	var loginuser = localStorage.getItem('OffUser');
+	var loginpw = localStorage.getItem('OffPassword'); 
+	
+	$.ajax({
+  	 type: 'POST',
+ 	 url: '../offspick/',
+ 	 data: {'func': 'offSpickCheckLogin', 'user': loginuser, 'pw': loginpw},
+ 	 success: function(data){
+		 					console.log(data);
+							if (data.success === true){
+								
+							requestOffSpickList();
+							
+								}
+								else {
+									// ggf. Alert durch Popup ersetzen
+									alert("Passwort falsch");
+									$.mobile.changePage("#loginForm"); 
+									}
+							},
+	 error: function(request, text, errorObj){							
+							// entspricht dem Fall "Server nicht erreichbar" -> Lokale Daten prüfen ob vorhanden und ohne weitere Authentifizierung anzeigen
+							showOffSpickOffline()
+							}
+	 });
+}
+
+
+// ***************************************************************************************************
+// Daten vom Server abrufen. Bei Fehler den letzten lokalen Stand anzeigen
+// ***************************************************************************************************
+
+function requestOffSpickList(){
+	
+	$.ajax({
+  	 type: 'POST',
+ 	 url: '../offspick/',
+ 	 data: {'func': 'offSpickList'},
+ 	 success: function(data){
+		 					if (data.success === true && data.result.length>0){
+								console.log("versuche Daten abzuspeichern");
+								localStorage.setItem("OffSpickList", JSON.stringify(data.result)); 
+								showOffSpickOffline()
+							}
+							else {
+								alert("Fehler - lokale Daten werden verwendet");
+								showOffSpickOffline()
+								}
+							},
+ 	 error: function(request, text, errorObj){
+		 					alert("Fehler - lokale Daten werden verwendet");
+							showOffSpickOffline()
+	 						}
+	 });
+}
+
+
+// ***************************************************************************************************
+// Wenn Daten im LocalStorage vorhanden sind, die Anzeige starten, sonst Fehler ausgeben
+// ***************************************************************************************************
+
+function showOffSpickOffline() {
+
+	try {
+		var param = JSON.parse(localStorage.getItem("OffSpickList"));
+		offSpickListInsert(param);
+	}
+	catch (e) {
+		alert("Fehler - keine lokale Daten vorhanden");
+	}
+}
